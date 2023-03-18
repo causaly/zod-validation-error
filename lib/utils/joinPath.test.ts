30ff241cc94342398b78f51a609720120d6b50e7
Dimitrios C. Michalakos <dimitris@jmike.gr>
2023-03-18 08:54:24 +0200

    fix: increase coverage of JS identifiers in joinPath function
    Fixes #18

--- lib/utils/joinPath.test.ts
+++ lib/utils/joinPath.test.ts
@@ -13,11 +13,35 @@ describe('joinPath()', () => {
     expect(joinPath(['a', 'b', 'c'])).toEqual('a.b.c');
   });
 
+  test('handles ideograms', () => {
+    expect(joinPath(['你好'])).toEqual('你好');
+  });
+
+  test('handles nested object path with ideograms', () => {
+    expect(joinPath(['a', 'b', '你好'])).toEqual('a.b.你好');
+  });
+
   test('handles numeric index', () => {
     expect(joinPath([0])).toEqual('[0]');
   });
 
   test('handles nested object path with numeric indices', () => {
     expect(joinPath(['a', 0, 'b', 'c', 1, 2])).toEqual('a[0].b.c[1][2]');
+  });
+
+  test('handles special characters', () => {
+    expect(joinPath(['exports', './*'])).toEqual('exports["./*"]');
+  });
+
+  test('handles quote corner-case', () => {
+    expect(joinPath(['a', 'b', '"'])).toEqual('a.b["\\""]');
+  });
+
+  test('handles quoted values', () => {
+    expect(joinPath(['a', 'b', '"foo"'])).toEqual('a.b["\\"foo\\""]');
+  });
+
+  test('handles unicode characters', () => {
+    expect(joinPath(['a', 'b', '💩'])).toEqual('a.b["💩"]');
   });
 });
--- lib/utils/joinPath.ts
+++ lib/utils/joinPath.ts
@@ -1,10 +1,27 @@
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#identifiers
+ */
+const identifierRegex = /[$_\p{ID_Start}][$\u200c\u200d\p{ID_Continue}]*/u;
+
 export function joinPath(arr: Array<string | number>): string {
   return arr.reduce<string>((acc, value) => {
+    // handle numeric indices
     if (typeof value === 'number') {
       return acc + '[' + value + ']';
     }
 
-    const separator = acc === '' ? '' : '.';
+    // handle quoted values
+    if (value.includes('"')) {
+      return acc + '["' + value.replace(/"/g, '\\"') + '"]';
+    }
+
+    // handle special characters
+    if (!identifierRegex.test(value)) {
+      return acc + '["' + value + '"]';
+    }
+
+    // handle normal values
+    const separator = acc.length === 0 ? '' : '.';
     return acc + separator + value;
   }, '');
 }
