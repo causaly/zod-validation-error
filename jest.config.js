9640ec7162d7c09984606e9e367b4e90d843b004
Thanos Karagiannis <thanoskrg@gmail.com>
2023-10-31 09:20:03 +0200

    chore(deps): rollback to prettier@v2 for jest compat

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   collectCoverageFrom: ['lib/**/*.{js,ts}'],
   testRegex: './lib/.*\\.(test|spec)\\.(js|ts)$',
-  /**
-   * @see https://jestjs.io/docs/configuration/#prettierpath-string
-   */
-  prettierPath: require.resolve('prettier-2'),
 };
