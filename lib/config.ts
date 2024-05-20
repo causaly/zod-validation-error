c22f2daad3250456f7e0e2f6d74c584d0bb7a662
Dimitrios C. Michalakos <dimitris@jmike.gr>
2024-05-20 19:47:15 +0300

    feat: add default value for MAX_ENUM_VALUES in config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -3,3 +3,4 @@ export const MAX_ISSUES_IN_MESSAGE = 99; // I've got 99 problems but the b$tch a
 export const PREFIX = 'Validation error';
 export const PREFIX_SEPARATOR = ': ';
 export const UNION_SEPARATOR = ', or ';
+export const MAX_ENUM_VALUES = Number.MAX_SAFE_INTEGER;
