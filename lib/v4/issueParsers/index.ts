17a0fd2add481b40c5ee7e2ca007f3e845dc3b63
Dimitrios C. Michalakos <dimitris@jmike.gr>
2025-05-28 16:10:30 +0300

    refactor: move issue parsers to their own files

---
+++ lib/v4/issueParsers/index.ts
@@ -0,0 +1,22 @@
+import { parseInvalidStringFormatIssue } from './invalidStringFormat.ts';
+import { parseInvalidTypeIssue } from './invalidType.ts';
+import { parseInvalidValue } from './invalidValue.ts';
+import { parseTooBigIssue } from './tooBig.ts';
+import { parseTooSmallIssue } from './tooSmall.ts';
+import type {
+  IssueType,
+  AbstractSyntaxTree,
+  IssueParseOptions,
+} from './types.ts';
+
+export const issueParsers: Record<
+  IssueType,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (issue: any, options: IssueParseOptions) => AbstractSyntaxTree
+> = {
+  invalid_type: parseInvalidTypeIssue,
+  too_big: parseTooBigIssue,
+  too_small: parseTooSmallIssue,
+  invalid_format: parseInvalidStringFormatIssue,
+  invalid_value: parseInvalidValue,
+};
---
+++ lib/v4/issueParsers/invalidValue.ts
@@ -0,0 +1,41 @@
+import { joinValues } from '../../utils/joinValues.ts';
+import { stringifyValue } from '../../utils/stringify.ts';
+import type { AbstractSyntaxTree, IssueParseOptions } from './types.ts';
+import type * as zod from 'zod/v4/core';
+
+export function parseInvalidValue(
+  issue: zod.$ZodIssueInvalidValue,
+  options: Pick<
+    IssueParseOptions,
+    | 'valuesSeparator'
+    | 'wrapStringValuesInQuote'
+    | 'maxValuesToDisplay'
+    | 'valuesLastSeparator'
+  >
+): AbstractSyntaxTree {
+  let expectation: string | undefined;
+
+  if (issue.values.length === 0) {
+    expectation = undefined;
+  } else if (issue.values.length === 1) {
+    const valueStr = stringifyValue(issue.values[0], {
+      wrapStringsInQuote: true,
+    });
+    expectation = `expected ${valueStr}`;
+  } else {
+    const valuesStr = joinValues(issue.values, {
+      separator: options.valuesSeparator,
+      lastSeparator: options.valuesLastSeparator,
+      wrapStringsInQuote: options.wrapStringValuesInQuote,
+      maxValuesToDisplay: options.maxValuesToDisplay,
+    });
+    expectation = `expected one of ${valuesStr}`;
+  }
+
+  return {
+    type: issue.code,
+    path: issue.path,
+    claim: 'invalid value',
+    expectation: expectation,
+  };
+}
