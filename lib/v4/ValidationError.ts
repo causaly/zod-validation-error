22fad09ee285001b0e0ea19e18523adc2e71ae23
Dimitrios C. Michalakos <dimitris@jmike.gr>
2025-05-28 10:26:14 +0300

    feat: introduce v4 error map (WIP)

---
+++ lib/v4/ValidationError.ts
@@ -0,0 +1,38 @@
+import { isZodErrorLike } from './isZodErrorLike.ts';
+import * as zod from 'zod/v4/core';
+
+// make zod-validation-error compatible with
+// earlier to es2022 typescript configurations
+// @see https://github.com/causaly/zod-validation-error/issues/226
+export interface ErrorOptions {
+  cause?: unknown;
+}
+
+export class ValidationError extends Error {
+  name: 'ZodValidationError';
+  details: Array<zod.$ZodIssue>;
+
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
+    this.name = 'ZodValidationError';
+    this.details = getIssuesFromErrorOptions(options);
+  }
+
+  toString(): string {
+    return this.message;
+  }
+}
+
+function getIssuesFromErrorOptions(
+  options?: ErrorOptions
+): Array<zod.$ZodIssue> {
+  if (options) {
+    const cause = options.cause;
+
+    if (cause instanceof zod.$ZodError) {
+      return cause.issues;
+    }
+  }
+
+  return [];
+}
