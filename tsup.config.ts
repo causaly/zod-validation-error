8d2f12ab90b6012df47b4791c91b2760278e3bdc
Dimitrios C. Michalakos <dimitris@jmike.gr>
2025-06-14 09:50:33 +0300

    chore: drop previous commented-out tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,5 @@
 import { defineConfig } from 'tsup';
 
-// export default defineConfig({
-//   entry: {
-//     'v3/index': 'lib/v3/index.ts',
-//     'v4/index': 'lib/v4/index.ts',
-//   },
-//   splitting: false,
-//   sourcemap: true,
-//   clean: true,
-//   format: ['cjs', 'esm'],
-//   dts: true,
-//   outDir: '.',
-// });
-
 export default [
   defineConfig({
     entry: ['lib/v3/index.ts'],
