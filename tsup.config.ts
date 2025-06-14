d73cb4f44fa656b9e4bbf50b117ecd30ff28d6fd
Dimitrios C. Michalakos <dimitris@jmike.gr>
2025-06-14 09:46:51 +0300

    feat: build files under ./v3 and ./v4 directories
    ...for compatibility with older node versions
    and module-resolution strategies.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,35 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: {
-    index: 'lib/index.ts',
-    'v4/index': 'lib/v4/index.ts',
-  },
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  format: ['cjs', 'esm'],
-  dts: true,
-  outDir: 'dist',
-});
+// export default defineConfig({
+//   entry: {
+//     'v3/index': 'lib/v3/index.ts',
+//     'v4/index': 'lib/v4/index.ts',
+//   },
+//   splitting: false,
+//   sourcemap: true,
+//   clean: true,
+//   format: ['cjs', 'esm'],
+//   dts: true,
+//   outDir: '.',
+// });
+
+export default [
+  defineConfig({
+    entry: ['lib/v3/index.ts'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    format: ['cjs', 'esm'],
+    dts: true,
+    outDir: 'v3',
+  }),
+  defineConfig({
+    entry: ['lib/v4/index.ts'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    format: ['cjs', 'esm'],
+    dts: true,
+    outDir: 'v4',
+  }),
+];
