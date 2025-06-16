c9b605e94b88dc38cf5f3a2422601e04a8df8762
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-06-16 13:55:41 +0800

    fix(v4): include paths of sub issues in union

--- lib/v4/errorMap/invalidUnion.test.ts
+++ lib/v4/errorMap/invalidUnion.test.ts
@@ -17,7 +17,36 @@ describe('parseInvalidUnionIssue', () => {
     if (result.success) throw new Error('Expected failure');
 
     expect(result.error.issues[0].message).toMatchInlineSnapshot(
-      `"Expected string or Expected number or Expected boolean or Expected null or Expected array or Expected record at "input""`
+      `"Expected string at "input" or Expected number at "input" or Expected boolean at "input" or Expected null at "input" or Expected array at "input" or Expected record at "input""`
+    );
+  });
+
+  test('handle nested errors', () => {
+    const baz = zod.number().min(1).optional();
+
+    const bar = zod.strictObject({
+      baz,
+    });
+
+    const foo = zod.strictObject({
+      bar: zod.literal(false).or(bar).optional(),
+    });
+
+    const schema = zod.object({
+      foo,
+    });
+
+    const result = schema.safeParse({
+      foo: {
+        bar: {
+          baz: 0,
+        },
+      },
+    });
+    if (result.success) throw new Error('Expected failure');
+
+    expect(result.error.issues[0].message).toMatchInlineSnapshot(
+      `"Expected value to be false at "foo.bar" or Number must be greater or equal to 1 at "foo.bar.baz""`
     );
   });
 });
