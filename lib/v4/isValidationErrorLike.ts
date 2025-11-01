693ac961b0851dfdb3d3c005cdd6fc6084c523a2
Dimitrios C. Michalakos <dimitris@jmike.gr>
2025-11-01 09:44:52 +0200

    chore: completely drop the v4 directory

--- lib/v4/isValidationErrorLike.ts
+++
@@ -1,8 +0,0 @@
-import {
-  ZOD_VALIDATION_ERROR_NAME,
-  type ValidationError,
-} from './ValidationError.ts';
-
-export function isValidationErrorLike(err: unknown): err is ValidationError {
-  return err instanceof Error && err.name === ZOD_VALIDATION_ERROR_NAME;
-}
